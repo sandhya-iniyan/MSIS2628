dec61917ea97a172a256260100a19dfc5966fa1e
Yue Ma <plenilune.m@gmail.com>
2022-10-18 15:31:54 -0700

    updated the logic between consumer.py and api.py

--- week4/consumer/code/consumer.py
+++ week4/consumer/code/consumer.py
@@ -1,38 +1,22 @@
+#import urllib.request
 from flask import Flask, render_template
-import random
 import json
 import os
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def recommend_meal():
-    menu = {
-        "Mionetto Prosecco Brut, Treviso, Veneto, Italy NV": "12.50",
-        "Mumm Brut Rosé, Napa Valley, California NV": "16.50",
-        "Taittinger 'La Francaise' Brut, Reims, Champagne NV": "22.00",
-        "Terlan Pinot Grigio, Alto Adige, Italy 2021": "14.00",
-        "C. Smith Riesling 'Kung Fu Girl', Columbia Valley, WA 2020": "12.00",
-        "Onx Sauvignon Blanc 'Field Day', Templeton Gap, CA 2020": "13.00",
-        "Left Coast Cellars White Pinot Noir, Willamette Valley, OR 2020":
-        "14.50",
-        "Licia Albariño, Rías Baixas, Spain 2021": "12.50",
-        "Groth Vineyards Sauvignon Blanc, Napa Valley, California 2021":
-        "15.50",
-        "Craggy Range Sauvignon Blanc Te Muna Rd, Martinborough, NZ 2021":
-        "14.00",
-        "Sean Minor Chardonnay, Sonoma Coast, CA 2019": "12.50",
-        "Garnier & Fils Chardonnay, Petit Chablis, Chablis, France 2020":
-        "16.00",
-        "Truchard Vineyards Chardonnay, Carneros, CA 2020": "17.00",
-        "Jordan Chardonnay, Russian River, Sonoma, California 2019": "19.00",
-        "The Beach by Whispering Angel, Aix-en-Provence, France 2021": "12.50",
-    }
-
-    meal, price = random.choice(list(menu.items()))
+def show_meal():
+    
+    url = os.environ.get("API_ENDPOINT")
+    response = requests.get(url)
+    data = response.content
+    meal = data[0]
+    price = data[1]   
     return render_template('index.html', meal=meal, price=price)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get("CONSUMER_PORT"))
